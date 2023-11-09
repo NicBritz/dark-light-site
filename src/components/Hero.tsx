3d192534555b5341d6dad3ab81d7fa43fa8e8f91
Nico Britz <nico.britz@outlook.com>
2023-11-09 18:12:12 +0000

    fix homepage spacing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,26 +7,32 @@ export function Hero() {
   return (
     <div className="hero">
       <div className="hero-content">
-        <div className="max-w-xl space-y-6">
+        <div className="max-w-xl space-y-12">
           <UiHeading>
-            Hello there, <br></br>I&apos;m Nico
+            Hello there, <br></br>I&apos;m{" "}
+            <span className="text-accent">Nico</span>
           </UiHeading>
-          <UiText>
-            For as long as I can remember, it was my dream to be a software
-            developer. I spent years studding (and still do) while maintaining a
-            full-time job among my other responsibilities.
-          </UiText>
-          <UiText>
-            Nowadays, I am proud to be a Full-stack developer, creating web
-            apps, websites and even games (sometimes just for fun :)
-          </UiText>
-          <Socials />
-          <Link
-            href="/portfolio"
-            className="btn btn-primary text-white uppercase"
-          >
-            Explore my work
-          </Link>
+
+          <div className="space-y-6">
+            <UiText>
+              For as long as I can remember, it was my dream to be a software
+              developer. I spent years studding (and still do) while maintaining
+              a full-time job among my other responsibilities.
+            </UiText>
+            <UiText>
+              Nowadays, I am proud to be a Full-stack developer, creating web
+              apps, websites and even games (sometimes just for fun :)
+            </UiText>
+          </div>
+          <div className="space-y-4">
+            <Socials />
+            <Link
+              href="/portfolio"
+              className="btn btn-primary text-white uppercase hover:opacity-80"
+            >
+              Explore my work
+            </Link>
+          </div>
         </div>
       </div>
     </div>
