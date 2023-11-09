f314923a0297d5d3b277ef400123e98b4736d942
Nico Britz <nico.britz@outlook.com>
2023-11-09 16:51:55 +0000

    Add portfolio projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,123 @@
+export const data = [
+  {
+    id: 1,
+    title: "Password Generator",
+    shortDescription:
+      "A password generator that generates a random password based on user-selected criteria.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699368380/Dark-Lightv4/password_gen_tpt5s1.webp",
+    detailedDescription:
+      "Random passwords are hard to guess and harder for computer programs to crack. I created this responsive password generator with a copy button to help with this. The mockup designs are from Frontend Mentor.",
+    tech: ["HTML", "CSS", "JavaScript", "React"],
+    live: "https://password-generator-app-lilac.vercel.app/",
+    github: "https://github.com/NicBritz/password-generator-app",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699368661/Dark-Lightv4/image_hg9oi7.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699368796/Dark-Lightv4/image_e8qgod.webp",
+    ],
+  },
+  {
+    id: 2,
+    title: "Turn Games",
+    shortDescription:
+      "A fictional e-commerce digital video game store built with Django.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437656/Dark-Lightv4/image_y7xn1b.webp",
+    detailedDescription:
+      "turn-games is an e-commerce digital video game store, the site offers a wide variety of visual entertainment content. Users can browse the sites extensive inventory and filter down the content for easier navigation. Users are also able to search the site by the use of a search bar that is always readily available. The landing page will show any content that the site owner has added to the featured or discounted sections. It will randomly select ten titles to display on the main page image slider. The site is no longer hosted due to deployment costs.",
+    tech: ["Django", "SQL", "Stripe", "Python"],
+    github: "https://github.com/NicBritz/turn-games",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437771/Dark-Lightv4/image_vtcopy.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437807/Dark-Lightv4/image_qwcwed.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437845/Dark-Lightv4/image_dldpno.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437896/Dark-Lightv4/image_vghsxi.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437934/Dark-Lightv4/image_xeu5aw.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699437968/Dark-Lightv4/image_qnwbwz.webp",
+    ],
+  },
+  {
+    id: 3,
+    title: "Notify",
+    shortDescription:
+      "A clone of a popular music application written as a web app.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699445139/Dark-Lightv4/image_nfgpqu.webp",
+    detailedDescription:
+      "Notify is a clone of a popular music application written as a web app. It has been written using Next.js and is a fully authenticated Fullstack application. The application is no longer hosted due to deployment costs.",
+    tech: ["TypeScript", "Next.js", "Prisma", "Framer M"],
+    github: "https://github.com/NicBritz/fullstack-notify-player",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699445835/Dark-Lightv4/image_oqywxl.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699445863/Dark-Lightv4/image_kphzbq.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699445904/Dark-Lightv4/image_chme14.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699445954/Dark-Lightv4/image_rtqutt.webp",
+    ],
+  },
+  {
+    id: 4,
+    title: "Lightrix",
+    shortDescription:
+      "A website for a South African business that specializes in repairing lighting and sound equipment.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447045/Dark-Lightv4/image_phzauy.webp",
+    detailedDescription:
+      "With over 10 years of experience, repairing and supporting entertainment equipment, you can rest assured your gear is in reliable hands. We strive to have the fastest turn around time on repairs and maintenance leaving you with minimal down time. Working with them to design and create this website was my pleasure.",
+    tech: ["Tailwindcss", "Sveltekit", "Cloudinary", "JavaScript"],
+    live: "https://lightrix.tech",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447092/Dark-Lightv4/image_doacbx.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447132/Dark-Lightv4/image_hx4lzk.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447175/Dark-Lightv4/image_w98zl3.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447208/Dark-Lightv4/image_pjlovf.webp",
+    ],
+  },
+  {
+    id: 5,
+    title: "Notable Shipping & Removals",
+    shortDescription:
+      "A website for a local business that specializes in shipping and removals.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699447567/Dark-Lightv4/image_ilh59n.webp",
+    detailedDescription:
+      "Notable Shipping and Removals will assist you with all your shipping and removals. From international shipping, excess baggage shipments, vehicle shipments, and domestic moves. It was a pleasure working with the director to design and develop this site for them. The site is fully responsive and even includes a volume calculator. Working with them to design and create this website was my pleasure.",
+    tech: ["Tailwindcss", "Sveltekit", "Cloudinary", "JavaScript"],
+    live: "https://notableshipping.co.uk",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458328/Dark-Lightv4/image_muxbis.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458370/Dark-Lightv4/image_htfah0.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458398/Dark-Lightv4/image_seaopw.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458424/Dark-Lightv4/image_pvuif1.webp",
+    ],
+  },
+  {
+    id: 6,
+    title: "Simon Game",
+    shortDescription:
+      "A clone of the popular 80's game Simon, built with JavaScript.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458561/Dark-Lightv4/image_e0ovwc.webp",
+    detailedDescription:
+      "Simon Game is a digital version of the popular 80's game Simon. The game is a memory game where the player has to remember the sequence of lights and sounds that the game plays. The game will start with one light and sound and will increase the sequence by one each time the player gets the sequence correct. The game will end when the player gets the sequence wrong. The game is fully responsive and will work on all devices.",
+    tech: ["JQuery", "CSS", "HTML", "JavaScript"],
+    live: "https://nicbritz.github.io/Milestone-2---Simon-Game/",
+    github: "https://github.com/NicBritz/Milestone-2---Simon-Game",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458854/Dark-Lightv4/image_ku0xpb.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458881/Dark-Lightv4/image_rcvhhk.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458931/Dark-Lightv4/image_nnshxj.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699458974/Dark-Lightv4/image_s118yo.webp",
+    ],
+  },
+  {
+    id: 7,
+    title: "Homechopped",
+    shortDescription:
+      "A recipe website that allows users to create, edit, and delete recipes.",
+    img: "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699459897/Dark-Lightv4/image_m9slyq.webp",
+    detailedDescription:
+      "HomeChopped is a FullStack, user-driven recipe website where you can share your ideas for delicious dishes. You can also rate recipes offered by other users and even add recipes to your collection of favorites for easy recall at a later stage. The site is fully responsive and will work on all devices. The site is no longer hosted due to deployment costs.",
+    tech: ["MongoDB", "Flask", "Python", "Heroku"],
+    github: "https://github.com/NicBritz/homechopped",
+    screenshots: [
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699460148/Dark-Lightv4/image_gytisk.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699460178/Dark-Lightv4/image_g6zhyg.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699460207/Dark-Lightv4/image_jlajpu.webp",
+      "https://res.cloudinary.com/dajuujhvs/image/upload/q_auto/v1699460236/Dark-Lightv4/image_gw25g3.webp",
+    ],
+  },
+];
